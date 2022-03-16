afee26909a7905ea407ed485d6b4c2adf4489cfc
Александр <cawasret13@mail.ru>
2022-03-16 19:22:33 +0300

    дз 2

--- dz2/dz2.c
+++ dz2/dz2.c
@@ -3,31 +3,49 @@
 
 int main()
 {
-    FILE *f1, *f2;
-    char arr[1001][1001],s;
-    int i = 0, j = 0;
-    f1 = fopen("adjacency_matrix_94.txt", "r");
-    while(!feof(f1)){
-      fscanf(f1, "%c", &s);
-      if(s!=' ' && s!='\n'){
-        arr[i][j] = s;
-        j++;
-        if(j==5){
-          i++;
-          j = 0;
+  FILE *f1, *f2;
+  char arr[1001][1001], s;
+  int i = 0, j = 0;
+  f1 = fopen("matrix_of_incendence13.txt", "r");
+  while (!feof(f1))
+  {
+    fscanf(f1, "%c", &s);
+    if (s != ' ' && s != '\n')
+    {
+      arr[i][j] = s;
+      j++;
+      if (j == 20)
+      {
+        i++;
+        j = 0;
+      }
+    }
+  }
+  char u;
+  f2 = fopen("res.txt", "w");
+  fprintf(f2, "graph grath {\n");
+  for (int a = 0; a < 20; a++)
+  {
+    u = -1;
+    for (int y = 0; y < 5; y++)
+    {
+      if (arr[y][a] != '0')
+      {
+        if (u == -1)
+        {
+          fprintf(f2, "%d", y + 1);
+          printf("%d", y + 1);
+          u = 0;
+        }
+        else
+        {
+          printf(" -- %d", y + 1);
+          fprintf(f2, " -- %d", y + 1);
         }
       }
     }
-  f2 = fopen("res.txt", "w");
-  fprintf(f2, "graph grath {\n");
-  for(int a =0;a<5;a++){
-      for(int y =0;y<5;y++){
-        if(arr[a][y]!='0'){
-          printf("%d - %d\n",y+1,a+1);
-          fprintf(f2, "  %d -- %d\n",a+1,y+1);
-        }
-      }
-    printf("\n");
+    fprintf(f2, ";\n");
+    printf(";\n");
   }
   fprintf(f2, "}");
   fclose(f2);
